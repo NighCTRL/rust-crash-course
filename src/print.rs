05d98e7ca3d7e67c2b95150559bcb00692d24216
nigh <code@nigh.one>
2022-12-13 16:47:23 +0100

    experiments with println!

---
+++ src/print.rs
@@ -0,0 +1,9 @@
+pub fn run() {
+    println!("Printing from the print.rs file");
+
+    // formatting
+    println!("This is basic {}", "formatting");
+
+    // Positional arguments
+    println!("{0} is playing {1} and {0} hates it", "Brandon", "Baseball");
+}
